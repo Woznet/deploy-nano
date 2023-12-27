d765d8f57d6d26fe77a4a6ee2c59498333d3b848
woz <walker.chris87@gmail.com>
2023-12-26 22:25:29 -0500

    12-26-23

--- ubuntu/config/profile.ps1
+++ ubuntu/config/profile.ps1
@@ -10,7 +10,6 @@ if (-not ($env:GHRUNNING)) {
     ContinuationPrompt            = '  '
     BellStyle                     = 'Visual'
     PredictionViewStyle           = 'ListView'
-
     HistorySearchCursorMovesToEnd = $true
     HistorySaveStyl               = 'SaveIncrementally'
     PredictionSource              = 'HistoryAndPlugin'
@@ -18,13 +17,42 @@ if (-not ($env:GHRUNNING)) {
 
   Set-PSReadLineOption @Params
 
-  Set-PSReadLineKeyHandler -Function MenuComplete -Chord 'Shift+Tab'
-
   Remove-PSReadLineKeyHandler -Chord 'Shift+Tab'
   Set-PSReadLineKeyHandler -Function MenuComplete -Chord 'Shift+Tab'
 
+$GetShortPath = {
+    if ($null -ne $Host.UI.RawUI.WindowSize.Width) {
+        $MaxPromptPath = [int]($Host.UI.RawUI.WindowSize.Width / 3)
+        $CurrPath = $PWD.Path -replace '^[^:]+::'
+        $DSC = [System.IO.Path]::DirectorySeparatorChar
+
+        if ($CurrPath.Length -ge $MaxPromptPath) {
+            $PathParts = $CurrPath.Split($DSC)
+            $EndPath = [System.Text.StringBuilder]::new()
+
+            for ($i = $PathParts.Length - 1; $i -gt 0; $i--) {
+                $TempPart = $PathParts[$i]
+                $TempPath = [System.IO.Path]::Combine($EndPath.ToString(), $TempPart)
+                if ($TempPath.Length -lt $MaxPromptPath) {
+                    [void]$EndPath.Insert(0, $TempPart + $DSC)
+                }
+                else {
+                    break
+                }
+            }
+            $GSPath = '{0}{1}...{1}{2}' -f $PathParts[0], $DSC, $EndPath.ToString().TrimEnd($DSC)
+        }
+        else {
+            $GSPath = $CurrPath
+        }
+        return $GSPath
+    }
+}
+
   function prompt {
-    [environment]::CurrentDirectory = $PWD.Path
+    if ($PWD.Provider.Name -eq 'FileSystem') {
+      [Environment]::CurrentDirectory = (Convert-Path -Path '.')
+    }
     $History = Get-History -Count 1 -ErrorAction Ignore
     if ($History) {
       $CmdExeTime = switch (New-TimeSpan -Start $History.StartExecutionTime -End $History.EndExecutionTime) {
@@ -41,8 +69,8 @@ if (-not ($env:GHRUNNING)) {
         '{0}[38;5;{4}m{{{0}[38;5;{1}m{7}{0}[38;5;{4}m}}{0}[0m'
       ) -f $esc, $lcyan, $yel, $grn, $dcyan,
       ('{0}{1}{2}' -f ([char]0xAB), ([DateTime]::Now.ToShortTimeString()), ([char]0xBB)),
-      [System.Net.Dns]::GetHostName(), ($PWD.Path -replace '^[^:]+::'), ([char]0xA7),
-      $CmdExeTime, [environment]::NewLine
+      [System.Net.Dns]::GetHostName(), $GetShortPath.Invoke().Trim(), ([char]0xA7),
+      $CmdExeTime, "`n"
     )
     return '> '
   }
