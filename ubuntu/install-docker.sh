433e68eaa7a7ce0e22619c633564ca6d564de6b7
Woz <15522036+Woznet@users.noreply.github.com>
2024-04-22 15:19:30 -0400

    Update install-docker.sh

--- ubuntu/install-docker.sh
+++ ubuntu/install-docker.sh
@@ -10,7 +10,7 @@ if [[ ! $(which docker) ]]; then
 
     # Add the repository to Apt sources:
     echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
         $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
         sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
     sudo apt update
