14d6f8f16f32aeb86d437601a0c0f7c854545238
Woz <15522036+Woznet@users.noreply.github.com>
2024-06-15 18:47:08 -0400

    Update build-nano.sh

--- ubuntu/build-nano.sh
+++ ubuntu/build-nano.sh
@@ -9,11 +9,11 @@ git clone https://github.com/galenguyer/nano-syntax-highlighting.git
 readlink -f ./nano-syntax-highlighting >/tmp/nanosyntaxpath.tmp
 
 cd ~/temp
-# wget https://www.nano-editor.org/dist/v7/nano-7.2.tar.xz
-wget https://raw.githubusercontent.com/Woznet/deploy-nano/main/ubuntu/nano-7.2.tar.xz
-tar -xf nano-7.2.tar.xz
-cd nano-7.2
-readlink -f . >/tmp/nanobuildpath.tmp
+# wget https://www.nano-editor.org/dist/v8/nano-8.0.tar.xz
+wget https://raw.githubusercontent.com/Woznet/deploy-nano/main/ubuntu/nano-8.0.tar.xz
+tar -xf nano-8.0.tar.xz
+cd nano-8.0
+readlink -f . >>/tmp/nanobuildpath.tmp
 
 ./configure --prefix=/usr \
   --sysconfdir=/etc \
@@ -22,23 +22,23 @@ readlink -f . >/tmp/nanobuildpath.tmp
   --enable-extra \
   --enable-nanorc \
   --enable-multibuffer \
-  --docdir=/usr/share/doc/nano-7.2 >/tmp/nano-config.log &&
-  make >/tmp/nano-make.log
+  --docdir=/usr/share/doc/nano-8.0 >>/tmp/nano-config.log &&
+  make >>/tmp/nano-make.log
 
 sudo -i
 cd $(cat /tmp/nanobuildpath.tmp)
 
-make install >/tmp/nano-makeinstall.log &&
-  install -v -m644 doc/{nano.html,sample.nanorc} /usr/share/doc/nano-7.2
+make install >>/tmp/nano-makeinstall.log &&
+  install -v -m644 doc/{nano.html,sample.nanorc} /usr/share/doc/nano-8.0 >>/tmp/nano-makeinstall.log
 
 cp /etc/nanorc /etc/nanorc.bak >/dev/null
 curl https://raw.githubusercontent.com/Woznet/deploy-nano/main/ubuntu/config/nanorc | tee /etc/nanorc >/dev/null
 
 mv --force $(cat /tmp/nanosyntaxpath.tmp)/*.nanorc /usr/share/nano/ >/dev/null
-chmod --changes =644 /usr/share/nano/*.nanorc
-chown --changes --recursive root:root /usr/share/nano/
+chmod --changes =644 /usr/share/nano/*.nanorc >/dev/null
+chown --changes --recursive root:root /usr/share/nano/ >/dev/null
 
-rm -v /tmp/*.tmp
+rm -v /tmp/*.tmp >/dev/null
 
 # Set up the default editor to be nano
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nano 1 &&
