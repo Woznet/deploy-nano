dc677c67bb92d9e042471788c3dc05d5403377ff
woz <walker.chris87@gmail.com>
2024-11-30 20:10:26 -0500

    Update non-critical-functions.sh

--- ubuntu/v3/non-critical-functions.sh
+++ ubuntu/v3/non-critical-functions.sh
@@ -94,7 +94,7 @@ install_vscode() {
     log "Starting installation of Visual Studio Code..."
     if [[ ! $(command -v code) ]]; then
         run_command "sudo apt-get install -y wget gpg"
-        run_command "wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg"
+        run_command "wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --yes --dearmor >packages.microsoft.gpg"
         run_command "sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg"
         run_command "sudo sh -c 'echo \"deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main\" > /etc/apt/sources.list.d/vscode.list'"
         run_command "rm -f packages.microsoft.gpg"
@@ -119,12 +119,12 @@ install_az() {
 install_1password() {
     log "Starting installation of 1Password..."
     if [[ ! $(command -v 1password) ]]; then
-        run_command "curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg"
+        run_command "curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --yes --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg"
         run_command "echo \"deb [arch=\$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/\$(dpkg --print-architecture) stable main\" | sudo tee /etc/apt/sources.list.d/1password.list"
         run_command "sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/"
         run_command "curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol"
         run_command "sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22"
-        run_command "curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg"
+        run_command "curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --yes --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg"
         run_command "sudo apt update"
         run_command "sudo apt install 1password"
         run_command "sudo apt install 1password-cli"
