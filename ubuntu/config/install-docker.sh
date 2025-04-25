4ef2c5c66c2bfe019b5a563f89c49a71ed862024
Woz <15522036+Woznet@users.noreply.github.com>
2025-04-25 15:34:25 -0400

    Update install-docker.sh

--- ubuntu/config/install-docker.sh
+++ ubuntu/config/install-docker.sh
@@ -3,7 +3,7 @@
 if [[ ! $(which docker) ]]; then
     # Add Docker's official GPG key:
     sudo apt update
-    sudo apt install ca-certificates curl
+    sudo apt install -y ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -16,7 +16,7 @@ if [[ ! $(which docker) ]]; then
     sudo apt update
 
     # Install Docker Engine
-    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # Setup root-less docker
     sudo groupadd docker
@@ -32,5 +32,5 @@ if [[ ! $(which docker) ]]; then
     # Download Docker Desktop deb file
     curl -L $DOCKER_DESKTOP_URL -o docker-desktop.deb
     # Install Docker Desktop
-    sudo apt install ./docker-desktop.deb
+    # sudo apt install ./docker-desktop.deb
 fi
