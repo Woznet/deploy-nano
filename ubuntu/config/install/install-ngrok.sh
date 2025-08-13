bca2ad518064a3d2f1964441600423c2df6a5938
Woz <15522036+Woznet@users.noreply.github.com>
2025-08-13 19:32:01 -0400

    Refactor ngrok installation script for improved readability and structure

--- ubuntu/config/install/install-ngrok.sh
+++ ubuntu/config/install/install-ngrok.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 if [[ ! $(which ngrok) ]]; then
-curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null &&
-    echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" | sudo tee /etc/apt/sources.list.d/ngrok.list &&
-    sudo apt update &&
-    sudo apt install -y ngrok
-
+    curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null &&
+        echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" | sudo tee /etc/apt/sources.list.d/ngrok.list &&
+        sudo apt update &&
+        sudo apt install -y ngrok
+    # Create ngrok bash completion file in /etc/bash_completion.d
+    if command -v ngrok >/dev/null 2>&1; then
+        sudo mkdir -p /etc/bash_completion.d
+        ngrok completion bash | sudo tee /etc/bash_completion.d/ngrok_completion >/dev/null
+        sudo chmod 644 /etc/bash_completion.d/ngrok_completion
+    fi
 fi
