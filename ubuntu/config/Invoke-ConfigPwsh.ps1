a6ffe7933e4bb78a48d85304adf1edc324d3388f
Woz <15522036+Woznet@users.noreply.github.com>
2025-08-15 18:56:35 -0400

    Update Invoke-ConfigPwsh.ps1

--- ubuntu/config/Invoke-ConfigPwsh.ps1
+++ ubuntu/config/Invoke-ConfigPwsh.ps1
@@ -3,20 +3,25 @@
 try {
     $ErrorActionPreference = 'Stop'
     if (Get-Module -ListAvailable Microsoft.PowerShell.PSResourceGet -ErrorAction SilentlyContinue) {
-        Import-Module Microsoft.PowerShell.PSResourceGet
-        Set-PSResourceRepository -Name PSGallery -Trusted
+        Import-Module Microsoft.PowerShell.PSResourceGet;
+        Set-PSResourceRepository -Name PSGallery -Trusted;
     }
     if (Get-Module -ListAvailable PowerShellGet -ErrorAction SilentlyContinue) {
-        Import-Module PowerShellGet
-        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+        Import-Module PowerShellGet;
+        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
     }
-    Install-PSResource -Name PSReadLine, Az.Accounts, Az.Tools.Predictor, Microsoft.PowerShell.PSResourceGet, WozTools -Reinstall -Scope AllUsers -PassThru
-    Update-Help -UICulture ([cultureinfo]::CurrentUICulture) -Module * -Force
+    Install-PSResource -Name PSReadLine, Az.Accounts, Az.Tools.Predictor, Microsoft.PowerShell.PSResourceGet, WozTools -Reinstall -Scope AllUsers -PassThru;
+    Update-Help -UICulture ([cultureinfo]::CurrentUICulture) -Module * -Force -ErrorAction Ignore;
+}
+catch {
+    Write-Error -ErrorRecord $_;
+}
+
+try {
     $Csv1 = (getent passwd) | ConvertFrom-Csv -Delimiter ':' -Header User, X, UserID, GroupID, Name, Home, App;
     $InvUser = $Csv1 | Where-Object {$_.UserID -eq $env:SUDO_UID};
     $InvUAH = Join-Path $InvUser.Home '.config/powershell/profile.ps1';
     $InvUCH = Join-Path $InvUser.Home '.config/powershell/Microsoft.PowerShell_profile.ps1';
-
     @(
         $PROFILE.AllUsersAllHosts,
         $PROFILE.AllUsersCurrentHost,
@@ -24,10 +29,10 @@ try {
         $PROFILE.CurrentUserCurrentHost,
         $InvUAH,
         $InvUCH
-    ) | Where-Object {(Test-Path $_) -eq $false} | ForEach-Object {$null = New-Item -Path $_ -ItemType File -Force -ErrorAction Continue}
-    if (Test-Path $InvUAH) { & chown "$($env:SUDO_USER):$($env:SUDO_USER)" $InvUAH }
-    if (Test-Path $InvUCH) { & chown "$($env:SUDO_USER):$($env:SUDO_USER)" $InvUCH }
+    ) | Where-Object {(Test-Path $_) -eq $false} | ForEach-Object {$null = New-Item -Path $_ -ItemType File -Force -ErrorAction Continue;}
+    if (Test-Path $InvUAH) { & chown "$($env:SUDO_USER):$($env:SUDO_USER)" $InvUAH;}
+    if (Test-Path $InvUCH) { & chown "$($env:SUDO_USER):$($env:SUDO_USER)" $InvUCH;}
 }
 catch {
-    throw $_
+    Write-Error -ErrorRecord $_;
 }
